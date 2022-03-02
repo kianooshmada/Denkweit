99cefc3dc8e20037698c886451491625f6d9babd
kianoosh mada <kianooshnike1365@gmail.com>
2022-03-02 09:45:35 +0330

    Create GraphQL Server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
+const schema = require("./schema");
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
